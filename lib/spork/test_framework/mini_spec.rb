77d48d2458ed1f8071fdc282bcb36ffb0bf82e76
Marlon Moyer <marlon@mcmoyer.com>
2011-10-20 10:07:29 -0500

    automatically append spec dir into load path

--- lib/spork/test_framework/mini_spec.rb
+++ lib/spork/test_framework/mini_spec.rb
@@ -1,3 +1,5 @@
 class Spork::TestFramework::MiniSpec < Spork::TestFramework::TestUnit
   HELPER_FILE = File.join(Dir.pwd, "spec/spec_helper.rb")
+  SPEC_PATH = File.join(Dir.pwd, "spec")
+  $LOAD_PATH << SPEC_PATH
 end
